0c1bd2673f8053842205edfb2b25e9cbfd3249ca
Donny Techiera <DonnyTech20@gmail.com>
2019-04-10 16:49:48 -0400

    stop timer and running timer firing

--- index.js
+++ index.js
@@ -1,38 +1,72 @@
-const testWrapper =
-document.querySelector(".test-wrapper");
-const testArea =
-document.querySelector("#test-area");
-const originText =
-document.querySelector("#origin-text p")
-.innerHTML;
-const resetButton =
-document.querySelector("#reset");
-const theTimer =
-document.querySelector(".timer");
+const testWrapper = document.querySelector('.test-wrapper');
+const testArea = document.querySelector('#test-area');
+const originText = document.querySelector('#origin-text p').innerHTML;
+const resetButton = document.querySelector('#reset');
+const theTimer = document.querySelector('.timer');
 
+// Array captures min, sec, hund, thous of seconds
+let timer = [ 0, 0, 0, 0 ];
+let interval;
+let timerRunning = false;
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-
+const leadingZero = (time) => {
+	if (time <= 9) {
+		time = '0' + time;
+	}
+	return time;
+};
 
 // Run a standard minute/second/hundredths timer:
+const runTimer = () => {
+	let currentTime = leadingZero(timer[0]) + ':' + leadingZero(timer[1]) + ':' + leadingZero(timer[2]);
+	theTimer.innerHTML = currentTime;
+	// Grabs the last value of timer array thousand of a sec
+	timer[3]++;
 
+	timer[0] = Math.floor(timer[3] / 100 / 60);
+	timer[1] = Math.floor(timer[3] / 100 - timer[0] * 60);
+	timer[2] = Math.floor(timer[3] - timer[1] * 100 - timer[0] * 6000);
+};
 
 // Match the text entered with the provided text on the page:
 const spellCheck = () => {
-    let textEntered = testArea.value;
-    console.log(textEntered);
-}
+	let textEntered = testArea.value;
+	let originTextMatch = originText.substring(0, textEntered.length);
+
+	if (textEntered === originText) {
+		clearInterval(interval);
+		textWrapper.style.borderColor = '#429890';
+	} else {
+		if (textEntered === originTextMatch) {
+			testWrapper.style.borderColor = '#65CCf3';
+		} else {
+			testWrapper.style.borderColor = '#FF0000';
+		}
+	}
+};
 
 // Start the timer:
 const start = () => {
-let textEnterdLength = testArea.value.length;
-   console.log(textEnterdLength);
-}
+	let textEnteredLength = testArea.value.length;
+	if (textEnteredLength === 0 && !timerRunning) {
+		timerRunning = true;
+		interval = setInterval(runTimer, 10);
+	}
+	console.log(textEnteredLength);
+};
 
 // Reset everything:
 const reset = () => {
-    console.log('You have fired up the reset button 💡');
-}
+	clearInterval(interval);
+	interval = null;
+	timer = [0,0,0,0];
+	timerRunning = false;
+
+	testArear.value = "";
+	theTimer.innerHTML = "00:00:00";
+	testWrapper.style.borderColor = "grey"; 
+};
 
 // Event listeners for keyboard input and the reset button:
 testArea.addEventListener('keypress', start, false);
